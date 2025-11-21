a97564c28ab7337821bccb29882d0e9b98b17443
btafarelo <btafarelo@gmail.com>
2025-11-21 17:32:29 +0100

    Keycloak and maven upgrade, postgres database, fix build.sh. Intenting of strip cookies by server side, not working yet.

--- tomcat/build.sh
+++ tomcat/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #apt update && apt install -y maven
 
@@ -10,12 +10,12 @@ unzip keycloak-oidc-tomcat-adapter-18.0.0.zip -d /usr/local/tomcat/lib
 
 rm keycloak-oidc-tomcat-adapter-18.0.0.zip
 
-curl https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz | tar -xz
+curl https://dlcdn.apache.org/maven/maven-3/3.9.11/binaries/apache-maven-3.9.11-bin.tar.gz | tar -xz
 
 cd /tmp/webapp
 
-/tmp/apache-maven-3.8.5/bin/mvn -T 1C clean package
+/tmp/apache-maven-3.9.11/bin/mvn -T 1C clean package
 
-cp -R target/webapp-1.0-SNAPSHOT /usr/local/tomcat/webapps/ROOT
+cp -R target/webapp-1.0-SNAPSHOT/ /usr/local/tomcat/webapps/ROOT
 
 exit 0
